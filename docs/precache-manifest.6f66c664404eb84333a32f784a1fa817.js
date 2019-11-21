472796cba3f7ce304882ebc3312b60ae489963b6
Phuong Nguyen Anh <phuong.nguyenanh@gameloft.com>
2019-11-21 17:02:46 +0700

    * built

---
+++ docs/precache-manifest.6f66c664404eb84333a32f784a1fa817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e49b5d2a963d2178a88d93cd6f4b80c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59520fb3f84f80539eff",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "71afd9c208b41507ad58",
+    "url": "/static/js/2.6e14b997.chunk.js"
+  },
+  {
+    "revision": "59520fb3f84f80539eff",
+    "url": "/static/js/main.79731cd0.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
